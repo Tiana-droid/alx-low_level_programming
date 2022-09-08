77dd7e3b1d1a7ac4e3cb5746a33c64c962b72122
Tiana-droid <adelusic@gmail.com>
2022-09-08 15:18:30 -0700

    cisfun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 
 /**
  * main - entry point
- * Description: prints the size of various data types on the computer it is compiled and run on
+ * Description: prints the size of various data types on the computer it is compiled
+ * and run on
  *
  * Return: Always 0 (Success)
  */
@@ -10,9 +11,9 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of a int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-       	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 	return (0);
 }
