6ef9047e041a436877da5d3c86935e2d145f6403
Tiana-droid <adelusic@gmail.com>
2022-09-09 10:48:50 -0700

    lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 97;
+
+	for(; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}
