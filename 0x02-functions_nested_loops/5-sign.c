3e1a5d85303015c2e3772ce3bc78b4817ea6a602
Tiana-droid <adelusic@gmail.com>
2022-09-13 12:11:09 -0700

    show your sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,21 +18,21 @@ int print_sign(int n)
 		_putchar(',');
 		_putchar(' ');
 		return (1);
+	}
 
-		else if (n = 0)
-		{
-			_putchar(0);
-			_putchar(',');
-			_putchar(' ');
-			return (0);
-		}
+	else if (n == 0)
+	{
+		_putchar(0);
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
 
-		else if (n < 0)
-		{
-			_putchar(45);
-			_putchar(',');
-			_putchar(' ');
-			return (-1);
-		}
+	else if (n < 0)
+	{
+		_putchar(45);
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
 	}
 }
