958863d94fb82f8b43e2eecd6a01d7bb98bfb32c
Tiana-droid <adelusic@gmail.com>
2022-09-13 11:56:59 -0700

    files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int_abs - entry point
+ *
+ * @c: parameter with type int
+ *
+ * Return: Always 0 (Success)
+ */
+
+int int_abs(int)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	return (c * -1);
+}	
