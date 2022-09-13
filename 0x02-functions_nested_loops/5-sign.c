2101497b59b8c158b04ca8820106b23b65a99e0e
Tiana-droid <adelusic@gmail.com>
2022-09-13 12:35:47 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,20 +18,20 @@ int print_sign(int n)
 		return (1);
 	}
 
-	else if (n == 0)
-	{
-		_putchar(0);
-		_putchar(',');
-		_putchar(' ');
-		return (0);
-	}
-
 	else if (n < 0)
 	{
 		_putchar(45);
 		_putchar(',');
 		_putchar(' ');
 		return (-1);
+	}
+
+	else
+	{
+		_putchar(0);
+		_putchar(',');
+		_putchar(' ');
+		return (0);
 	}
 
 }
