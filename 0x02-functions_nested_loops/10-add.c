fb25ac2348beafb98a6242f70a544a66fec81183
Tiana-droid <adelusic@gmail.com>
2022-09-13 19:43:41 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - entry point
+ *
+ * @a: parameter with type int
+ *
+ * @b: parameter with type int
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
