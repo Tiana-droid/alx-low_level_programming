b356442900ca8780d6902d81d4bc7a728d2e92f9
Tiana-droid <adelusic@gmail.com>
2022-09-13 19:44:25 -0700

    up to 98 and down

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - entry point
+ * @n: parameter with type int
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d ", n++);
+		}
+		printf("%d\n", n);
+	}
+}
