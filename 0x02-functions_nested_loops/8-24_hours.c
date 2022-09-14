4b7ca5de282bb46b45ac521acd0de11fec886853
Tiana-droid <adelusic@gmail.com>
2022-09-13 19:42:27 -0700

    24 hours a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - main entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hrs = 0;
+	int mins = 0;
+	int hrs_rem;
+	int mins_rem;
+
+	while (hrs <= 23)
+	{
+		while (min <= 59)
+		{
+			mins_rem = min % 10;
+			hrs_rem = hrs % 10;
+			_putchar(hrs / 10 + '0');
+			_putchar(hrs_rem + '0');
+			_putchar(':');
+			_putchar(mins / 10 + '0');
+			_putchar(mins_rem + '0');
+
+			min++;
+			_putchar('\n');
+		}
+
+		hrs++;
+		min = 0;
+	}
+}
