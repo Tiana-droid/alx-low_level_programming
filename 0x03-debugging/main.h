9987ba5ed498e8075ec1bf24571917ef1846f9d3
Tiana-droid <adelusic@gmail.com>
2022-09-15 03:49:50 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 void positive_or_negative(int i);
+int large_num(int, int, int);
+void print_remaining(int, int, int);
+int convert_day(int, int);
 
 #endif /*MAIN_H*/
