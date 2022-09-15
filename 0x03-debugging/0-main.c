2399f68cd6860c5229f098bee0750d45a0108781
Tiana-droid <adelusic@gmail.com>
2022-09-15 01:47:52 -0700

    positive_or_negative testing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - testing the function positve_or_negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	positive_or_negative();
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Testing of C files
