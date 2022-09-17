60e5f6c64190a7a744fe99d0ff319f2d4b2bac1a
Tiana-droid <adelusic@gmail.com>
2022-09-17 14:21:24 -0700

    print nums

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ * @: int a
+ * @b: int b
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	_putchar(res);
+	return (res);
+}	
