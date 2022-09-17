ff5e73da6f1944727c62a96b638e400d4afbe82a
Tiana-droid <adelusic@gmail.com>
2022-09-17 10:45:37 -0700

    digit?

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * _isdigit - checks for a digit
- *
- * Return 1
+ * @c: character
+ * Return: 1
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && <= 9)
+	if (c >= '0' && <= '9')
 	{
 		return (1);
 	}
