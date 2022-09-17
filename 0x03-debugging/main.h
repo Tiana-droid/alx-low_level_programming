96786718f06d71f22cb0c88d64b18d31695dbea5
Tiana-droid <adelusic@gmail.com>
2022-09-17 08:41:04 -0700

    largest num

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int large_num(int, int, int);
+int largest_number(int a, int b, int c);
 void print_remaining(int, int, int);
 int convert_day(int, int);
 
