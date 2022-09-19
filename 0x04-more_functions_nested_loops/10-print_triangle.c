a862d6709fc04613091482fe531c82ea02d4606b
Tiana-droid <adelusic@gmail.com>
2022-09-19 18:44:49 +0000

    print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle(int size)
+ *
+ * @size: parameter
+ *
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int hash, space, line = 0;
+
+	if (size > 0)
+	{
+		while (line < size)
+		{
+			for (space = size - 1; space > line; space--)
+			{
+				_putchar(' ');
+			}
+			for (hash = 0; hash < line + 1; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar(10);
+			line++;
+		}
+	}
+	else
+		_putchar(10);
+}
