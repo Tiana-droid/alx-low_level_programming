d73c104ad04e9f96f21f6660d564f2ab237801b9
Tiana-droid <adelusic@gmail.com>
2022-09-28 16:03:42 +0000

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,19 +2,34 @@
 
 /**
  * is_prime_number - prints 1 if an integer is a prime number, otherwise 0
- * @n: integer
+ * @n: num
  * Return: integer
  */
 
 int is_prime_number(int n)
 {
+	int start = n / 2
 
-	if (n % n == 0 && n / n == 1)
-	{
-		return (1);
-	}
-	else
+	if (n <= 1)
 	{
 		return (0);
 	}
+	return (is_prime(n, start));
+}
+
+/**
+ * is_prime returns i if  is prime
+ * @n: num
+ * @start: num to start checking
+ *
+ * Return: integer
+ */
+
+int is_prime(int n, int start)
+{
+	if (start <= 1)
+		return (1);
+	else if (n % start == 0)
+		return (0);
+	return (is_prime(n, start - 1));
 }
