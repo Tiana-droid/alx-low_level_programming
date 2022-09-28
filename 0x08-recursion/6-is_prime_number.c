68096de3f6368934341d694855fcc27bf4dd64d8
Tiana-droid <adelusic@gmail.com>
2022-09-28 16:45:24 +0000

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,28 +8,12 @@
 
 int is_prime_number(int n)
 {
-	int start = n / 2;
 
 	if (n <= 1)
 	{
 		return (0);
 	}
-	return (is_prime(n, start));
-}
-
-/**
- * is_prime - returns 1 if  is prime
- * @n: num
- * @start: num to start checking
- *
- * Return: integer
- */
-
-int is_prime(int n, int start)
-{
-	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
-		return (0);
-	return (is_prime(n, start - 1));
+	else if (n / n = 1)
+		return (1)
+	return (0);
 }
