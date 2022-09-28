6d67983dd4f11cfade068ada7a460276a395f6e3
Tiana-droid <adelusic@gmail.com>
2022-09-28 15:23:35 +0000

    palindrome

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prints 1 if an integer is a prime number, otherwise 0
+ * @n: integer
+ * Return: integer
+ */
+
+int is_prime_number(int n)
+{
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == n && n / n == 1)
+		{
+			return (1);
+		}
+		else if (n % i != n || n / n == 1)
+		{
+			return (0)
+}	}
